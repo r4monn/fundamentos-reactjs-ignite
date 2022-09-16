9cc5835b398f7bdc4f5fc814af2fb12d8829bf00
R4mon <ramondiiaas@hotmail.com>
2022-09-16 20:04:06 -0300

    update

--- ignite-timer/src/components/Header/index.tsx
+++ ignite-timer/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ import { NavLink } from "react-router-dom";
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logoIgnite} alt=""/>
+      <img src={logoIgnite} alt="" />
       <nav>
         <NavLink to="/" title="Timer">
           <Timer size={24} />
