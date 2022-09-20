1726a56000423866fb6189811e324e3e0ecaaae7
R4mon <ramondiiaas@hotmail.com>
2022-09-20 16:40:33 -0300

    update contexts

--- ignite-timer/src/contexts/CyclesContext.tsx
+++ ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,3 +1,4 @@
+import { differenceInSeconds } from "date-fns";
 import { createContext, ReactNode, useEffect, useReducer, useState } from "react";
 import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
@@ -26,21 +27,39 @@ interface CyclesContextProviderProps {
 
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
 
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0')
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+    })
+
+  const { cycles, activeCycleId } = cyclesState
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)  // Verifica se tem ciclo/intervalo ativo
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(
+        new Date(),
+        new Date(activeCycle.startDate)
+      )
+    }
+
+    return 0
+  })
 
   useEffect(() => {
     const stateJSON = JSON.stringify(cyclesState)
 
     localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
   }, [cyclesState])
-
-  const { cycles, activeCycleId } = cyclesState
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)  // Verifica se tem ciclo/intervalo ativo
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
