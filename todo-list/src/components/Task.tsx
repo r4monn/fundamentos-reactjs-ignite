689396b133ca0f73bb4357d3d87f43a5bd4a44c9
R4mon <ramondiiaas@hotmail.com>
2022-11-02 15:33:52 -0300

    update tasks

--- todo-list/src/components/Task.tsx
+++ todo-list/src/components/Task.tsx
@@ -16,53 +16,29 @@ interface TaskProps {
 
 function Task({ task, id, onDeleteTask, onCompleteTask }: TaskProps) {
 
-  const isComplete = false;
-
   function handleDeleteTask() {
     onDeleteTask(task.id)
     console.log(task)
   }
-/*
-  function handleCheckCompletedTask(event: ChangeEvent<HTMLInputElement>) {
-    console.log('Check completed task')
-    
-    setIsChecked(isChecked => !isChecked);
-    onCheckTask(task.id)
-    onCompletedTasksCount()
 
-    if (event.target.checked) {
-      console.log('✅ Checkbox is checked');
-      
-      const findTask = tasks.find((task) => task.id === event.target.value)
-      findTask.isComplete = true;
-      setIsCheckedCount(isCheckedCount + 1)
-    } else {
-      console.log('⛔️ Checkbox is NOT checked');
-
-      const findTask = tasks.find((task) => task.id === event.target.value)
-      findTask.isComplete = false;
-      setIsCheckedCount(isCheckedCount - 1)
-    }
-  }
-*/
-  const labelClass = `${task.isComplete ? 'checked'  : ''}`
+  const labelClass = `${task.isComplete ? 'checked' : ''}`
 
   return (
     <div className="task">
-      <input 
-        type="checkbox" 
-        name="isCompleteRadio" 
+      <input
+        type="checkbox"
+        name="isCompleteRadio"
         id={`checkbox${id}`}
 
       />
-      <label htmlFor={`checkbox${id}`} className='checkbox' onClick={() => onCompleteTask(task.id) }/>
+      <label htmlFor={`checkbox${id}`} className='checkbox' onClick={() => onCompleteTask(task.id)} />
       <div className='titulo'>
         <label className={labelClass}>{task.title}</label>
       </div>
-      <button 
-        className='trash' 
+      <button
+        className='trash'
         onClick={handleDeleteTask}>
-          <Trash size={17} />
+        <Trash size={17} />
       </button>
     </div>
   );
